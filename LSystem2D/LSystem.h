a6147d6f3fa2532f812eda3bfa340b89a3713eb9
ai2playgame <detective9684@gmail.com>
2023-02-24 22:21:50 +0900

    インターフェース用意

---
+++ LSystem2D/LSystem.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class LSystem
+{
+public:
+	LSystem();
+
+	void Set(String token, void* command);
+
+	void Init(String start);
+	void Rule(String token, String replaced);
+	void Draw(uint32 state);
+};
+
