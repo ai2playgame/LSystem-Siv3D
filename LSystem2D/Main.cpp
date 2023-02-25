d1e413cce400b8fd1af07cb2612f75856f7bc9e5
ai2playgame <detective9684@gmail.com>
2023-02-25 23:26:25 +0900

    どう作るか考えた

--- LSystem2D/Main.cpp
+++ LSystem2D/Main.cpp
@@ -6,6 +6,27 @@ void Main()
 	Window::SetStyle(WindowStyle::Sizable);
 	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
 
+	/*
+		// 想定する使い方
+		LSystem lSystem {最初の直線のサイズを指定};
+		LSystem::Operator minusOperator;
+		minusOperator.Set(Lineを引数に取って、Vec2を返す関数オブジェクト);
+		lSystem.Set("-", minusOperator)
+
+		LSystem::Operator plusOperator;
+		plusOpeartor.Set(Lineを引数に取って、Vec2を返す関数オブジェクト);
+		lSystem.Set("+", plusOperator);
+
+		LSystem::Operator fOperator;
+		fOpeartor.Set(Lineを引数に取って、Vec2を返す関数オブジェクト);
+		lSystem.Set("F", fOperator);
+
+		lSystem.Init("F"); // 初期状態
+		lSystem.Rule("F", "F-F++F--F"); // Fをどう置き換えるか
+
+		lSystem.Draw(level);
+	*/
+
 	while (System::Update())
 	{
 	}
