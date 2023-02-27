55bddf37864ceaa34ac5d92b65f9877db39c89a0
ai2playgame <detective9684@gmail.com>
2023-02-27 23:51:40 +0900

    Turtleクラス準備

---
+++ LSystem2D/Turtle.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+namespace aiGraphics
+{
+	// タートルグラフィック
+	class Turtle
+	{
+	public:
+		Turtle(Vec2 start);
+
+		void Forward(float length);
+
+		void Right(float degree);
+		void Left(float degree) { Right(-degree); }
+	};
+}
